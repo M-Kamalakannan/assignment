b8c279ac3d041c1033d6e078db3d0d34b7db2601
Kamalakannan M <139243625+M-Kamalakannan@users.noreply.github.com>
2024-03-30 22:43:46 +0530

    Update landslidest.py

--- landslidest.py
+++ landslidest.py
@@ -1,5 +1,5 @@
 import streamlit as st
-X_test=[[ 0.77093807  ,0.00823664  ,0.35703567 , 2.45688651]]
+X_test=[[ 0.77093807,0.00823664,0.35703567,2.45688651]]
 st.header("logistic regression")
 import joblib
 mj = joblib.load('regression_model.joblib')
